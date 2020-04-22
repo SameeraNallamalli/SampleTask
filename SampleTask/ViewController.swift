86d0943396a558f779144d75be0b733edc8893e8
SameeraNallamalli <sameera.nallamalli@gmail.com>
2020-04-22 19:41:06 +0530

    Initial Commit

---
+++ SampleTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTask
+//
+//  Created by SAMEERA on 22/04/20.
+//  Copyright © 2020 Sameera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
